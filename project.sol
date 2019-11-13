75f513f8393edbd6f63dc26882655afbbb0020ca
brandon <brandonfox@hotmail.co.uk>
2019-11-13 19:23:42 +0700

    Added constraints to services: OUTSTANDING- Update records to support services/subservices

--- project.sol
+++ project.sol
@@ -22,24 +22,28 @@ contract BlockChainProject{
     
     mapping(uint8 => string) private services;
     
-    function containsService(string memory serviceName) internal view returns (bool){
+    function getIndexOfService(string memory serviceName) internal view returns (uint8) {
         bytes32 service = encode(serviceName);
         for(uint8 i = 0; i < 32; i++){
             if(bytes(services[i]).length > 0 && encode(services[i]) == service){
-                return true;
+                return i;
             }
         }
-        return false;
+        return 32;
+    }
+    
+    function containsService(string memory serviceName) internal view returns (bool){
+        return getIndexOfService(serviceName) < 32;
     }
     
     function addService(string memory serviceName) public payable ownerOnly returns (bool) {
         //Returns true if service was successfully added
         //False if service already exists
-        if(noOfServices >= 32 || containsService(serviceName)){
+        if(noOfServices >= 31 || containsService(serviceName)){
             return false;
         }
         else{
-            for(uint8 i = 0; i < 32; i++){
+            for(uint8 i = 0; i < 31; i++){
                 if(bytes(services[i]).length <= 0){
                     services[i] = serviceName;
                     noOfServices++;
@@ -50,16 +54,15 @@ contract BlockChainProject{
         return false;
     }
     
+    function editServiceName(uint8 serviceId, string memory newName) public payable ownerOnly returns (bool) {
+        services[serviceId] = newName;
+    }
+    
     function deleteService(string memory serviceName) public payable ownerOnly returns (bool) {
-        bytes32 service = encode(serviceName);
         if(containsService(serviceName)){
-            for(uint8 i = 0; i < 32; i++){
-                if(encode(services[i]) == service){
-                    services[i] = "";
-                    noOfServices--;
-                    return true;
-                }
-            }
+            services[getIndexOfService(serviceName)] = "";
+            noOfServices--;
+            return true;
         }
         return false;
     }
@@ -77,18 +80,38 @@ contract BlockChainProject{
     
     //-----------------------------_Subservices_----------------------------
     
-    mapping(uint32 => string[]) private subServices;
+    mapping(uint8 => string[]) private subServices;
+    
+    function getIndexOfSubService(uint8 serviceId, string memory serviceName) internal view returns (uint) {
+        for(uint8 i = 0; i < subServices[serviceId].length; i++){
+            if(encode(subServices[serviceId][i]) == encode(serviceName)){
+                return i;
+            }
+        }
+        return 255;
+    }
+    
+    function containsSubService(uint8 serviceId, string memory serviceName) internal view returns (bool) {
+        return getIndexOfSubService(serviceId,serviceName) < 255;
+    }
     
     function addSubService(uint8 serviceId, string memory newService) public payable ownerOnly returns (bool){
         //Returns true if service does not already exist
-        bytes32 service = encode(newService);
-        for(uint i = 0; i < subServices[serviceId].length; i++){
-            if(encode(subServices[serviceId][i]) == service){
-                return false;
-            }
+        if(subServices[serviceId].length >= 254 || containsSubService(serviceId,newService)){
+            return false;
         }
         subServices[serviceId].push(newService);
         return true;
+    }
+    
+    function editSubServiceName(uint8 serviceId, string memory oldName, string memory newName) public payable ownerOnly returns (bool) {
+        if(!containsSubService(serviceId,oldName)){
+            return false;
+        }
+        else{
+            subServices[serviceId][getIndexOfSubService(serviceId,oldName)] = newName;
+            return true;
+        }
     }
     
     function deleteSubService(uint8 serviceId, string memory serviceName) public payable ownerOnly returns (bool) {
@@ -123,10 +146,24 @@ contract BlockChainProject{
         require(verifiedDealers[dealerId]);
         _;
     }
+    function transferOwner(address newOwner) external payable ownerOnly{
+        owner = newOwner;
+    }
+
+    function verifyAddress(bytes32 adr) public payable ownerOnly{
+        verifiedDealers[adr] = true;
+    }
+    
+    function isVerified(bytes32 adr) public view returns (bool) {
+        return verifiedDealers[adr];
+    }
     
     function getId(string memory id) public pure returns (bytes32){
         return sha256(abi.encodePacked(id));
     }
+    
+    //------------------------------------------------------------_Records_---------------------------------------------
+
     
     function insertRecord(bytes32 dealerId, bytes32 id, uint32 servicesDone, string memory comment) public payable verified(dealerId){
         userRecords[id].push(Record(servicesDone,comment));
@@ -136,11 +173,5 @@ contract BlockChainProject{
         return userRecords[id];
     }
     
-    function transferOwner(address newOwner) external payable ownerOnly{
-        owner = newOwner;
-    }
 
-    function verifyAddress(bytes32 adr) public payable ownerOnly{
-        verifiedDealers[adr] = true;
-    }
 }
