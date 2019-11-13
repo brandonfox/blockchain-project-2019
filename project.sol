78b289cc286db41cb4bc5e67c255e158b5b546be
brandon <brandonfox@hotmail.co.uk>
2019-11-13 18:10:30 +0700

    Supported removing subservices

--- project.sol
+++ project.sol
@@ -10,6 +10,12 @@ contract BlockChainProject{
     
     address owner;
     
+    //---------------------------------_Meta-Functions_---------------------------------------
+    
+    function encode(string memory str) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(str));
+    }
+    
     //---------------------------------_Services_------------------------------------
     
     uint8 noOfServices;
@@ -17,9 +23,9 @@ contract BlockChainProject{
     mapping(uint8 => string) private services;
     
     function containsService(string memory serviceName) internal view returns (bool){
-        bytes32 service = keccak256(abi.encodePacked(serviceName));
+        bytes32 service = encode(serviceName);
         for(uint8 i = 0; i < 32; i++){
-            if(bytes(services[i]).length > 0 && keccak256(abi.encodePacked(services[i])) == service){
+            if(bytes(services[i]).length > 0 && encode(services[i]) == service){
                 return true;
             }
         }
@@ -45,10 +51,10 @@ contract BlockChainProject{
     }
     
     function deleteService(string memory serviceName) public payable ownerOnly returns (bool) {
-        bytes32 service = keccak256(abi.encodePacked(serviceName));
+        bytes32 service = encode(serviceName);
         if(containsService(serviceName)){
             for(uint8 i = 0; i < 32; i++){
-                if(keccak256(abi.encodePacked(services[i])) == service){
+                if(encode(services[i]) == service){
                     services[i] = "";
                     noOfServices--;
                     return true;
@@ -75,14 +81,25 @@ contract BlockChainProject{
     
     function addSubService(uint8 serviceId, string memory newService) public payable ownerOnly returns (bool){
         //Returns true if service does not already exist
-        bytes32 service = keccak256(abi.encodePacked(newService));
+        bytes32 service = encode(newService);
         for(uint i = 0; i < subServices[serviceId].length; i++){
-            if(keccak256(abi.encodePacked(subServices[serviceId][i])) == service){
+            if(encode(subServices[serviceId][i]) == service){
                 return false;
             }
         }
         subServices[serviceId].push(newService);
         return true;
+    }
+    
+    function deleteSubService(uint8 serviceId, string memory serviceName) public payable ownerOnly returns (bool) {
+        bytes32 service = encode(serviceName);
+        for(uint i = 0; i < subServices[serviceId].length; i++){
+            if(encode(subServices[serviceId][i]) == service){
+                delete subServices[serviceId][i];
+                return true;
+            }
+        }
+        return false;
     }
     
     function getSubService(uint8 serviceId) public view returns (string[] memory) {
@@ -92,7 +109,7 @@ contract BlockChainProject{
     //---------------------------------------------------------_End-Services_---------------------------------------------------
     
     mapping(bytes32 => Record[]) private userRecords;
-    mapping(address => bool) private verifiedDealers;
+    mapping(bytes32 => bool) private verifiedDealers;
     
     constructor() public {
         owner = msg.sender;
@@ -102,8 +119,8 @@ contract BlockChainProject{
         require(msg.sender == owner);
         _;
     }
-    modifier verified {
-        require(verifiedDealers[msg.sender]);
+    modifier verified(bytes32 dealerId) {
+        require(verifiedDealers[dealerId]);
         _;
     }
     
@@ -111,11 +128,11 @@ contract BlockChainProject{
         return sha256(abi.encodePacked(id));
     }
     
-    function insertRecord(bytes32 id, uint32 servicesDone, string memory comment) public payable verified{
+    function insertRecord(bytes32 dealerId, bytes32 id, uint32 servicesDone, string memory comment) public payable verified(dealerId){
         userRecords[id].push(Record(servicesDone,comment));
     }
     
-    function getRecords(bytes32 id) public view verified returns (Record[] memory){
+    function getRecords(bytes32 dealerId, bytes32 id) public view verified(dealerId) returns (Record[] memory){
         return userRecords[id];
     }
     
@@ -123,9 +140,7 @@ contract BlockChainProject{
         owner = newOwner;
     }
 
-    function verifyAddress(address adr) public payable ownerOnly{
+    function verifyAddress(bytes32 adr) public payable ownerOnly{
         verifiedDealers[adr] = true;
     }
-    
-
 }
