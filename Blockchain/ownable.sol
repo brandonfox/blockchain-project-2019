830ba31e205d8b46f355749e73ce19f9f2de872b
brandon <brandonfox@hotmail.co.uk>
2019-11-14 18:56:25 +0700

    Moved hash function to base contract

--- Blockchain/ownable.sol
+++ Blockchain/ownable.sol
@@ -6,16 +6,20 @@ contract Ownable {
     constructor() public {
         owner = msg.sender;
     }
-    
+
     address owner;
-    
+
     modifier ownerOnly{
-        require(msg.sender == owner,You are not the owner);
+        require(msg.sender == owner,"You are not the owner");
         _;
     }
     
+    function getHash(string memory id) public pure returns (bytes32){
+        return sha256(abi.encodePacked(id));
+    }
+
     function transferOwner(address newOwner) external payable ownerOnly{
         owner = newOwner;
     }
-    
+
 }
