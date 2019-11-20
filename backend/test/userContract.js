400c45af349f99e7529a8eefa16852fd189e1bfb
Sorawit Kongnurat <trainer.mapring@gmail.com>
2019-11-20 21:06:08 +0700

    replace with Brandon's test

--- backend/test/userContract.js
+++ backend/test/userContract.js
@@ -1,37 +1,71 @@
 const UserContract = artifacts.require('UserContract');
+const testCarPlate = 'NA34 87';
+const testCar = { brand: 'BMW', model: 'Series 3', year: '1997' };
 
 contract('UserContract', () => {
   let userContract = null;
+  userId = '';
   before(async () => {
     userContract = await UserContract.deployed();
+    userId = await userContract.getHash('TestUser');
   });
-
-  it('Should edit user info', async () => {
-    const userId = await userContract.getHash('32');
-    const userInfo = {
-      firstName: 'a',
-      lastName: 'b',
-      adr: 'c',
-      phNo: 'd',
-      email: 'e'
-    };
-    await userContract.editUserInfo(userId, userInfo);
-    const user = await userContract.getUserInfo(userId);
-    assert(user.firstName === 'a');
+  it('Should create car record', async () => {
+    await userContract.editCarDetails(userId, testCarPlate, testCar);
+    const carDetails = await userContract.getCars(userId);
+    assert(
+      carDetails[0].brand === testCar.brand &&
+        carDetails[0].model === testCar.model &&
+        carDetails[0].year === testCar.year,
+      'Record in blockchain[0] did not match testCar'
+    );
   });
-
-  it('Should insert record', async () => {
-    const dealerId = await userContract.getHash('31');
-    const userId = await userContract.getHash('32');
+  it('Should add user details', async () => {
+    await userContract.editUserInfo(userId, {
+      firstName: 'Brandon',
+      lastName: 'Fox',
+      adr: 'Thailand somewhere',
+      phNo: 'Yes',
+      email: 'brandon@email.email',
+    });
+    const infoReply = await userContract.getUserInfo(userId);
+    assert(
+      infoReply.firstName === 'Brandon',
+      'Record in the blockchain firstname did not match'
+    );
+  });
+  it('Should create a record', async () => {
+    const dealerId = await userContract.getHash('32');
     const dealerInfo = {
-      dealerName: 'test'
+      dealerName: 'test',
+      addr: 'mahidol',
+      location: '192.12312,24.12',
+      phoneNo: '081+++++++',
+      availableServices: [],
+      availableSubServices: [],
     };
     await userContract.createDealerApplication(dealerInfo, dealerId);
+    let application = await userContract.getAllDealerApplications();
+    assert(
+      application.includes(dealerId),
+      'Could not create dealer application'
+    );
     await userContract.approveApplication(dealerId);
     const verified = await userContract.isVerified(dealerId);
-    assert(verified);
-    await userContract.insertRecord(dealerId, userId, [], [], 'test');
-    const records = await userContract.getRecords(dealerId, userId);
-    assert(records[0].comment === 'test');
+    application = await userContract.getAllDealerApplications();
+    assert(
+      !application.includes(dealerId),
+      'Failed to delete approved dealer application from blockchain'
+    );
+    assert(verified, 'Dealer is still not verified');
+    await userContract.insertRecord(
+      dealerId,
+      userId,
+      testCarPlate,
+      [],
+      [],
+      'Done some test maintenance'
+    );
+    const records = await userContract.getRecords(userId, testCarPlate);
+    assert(records[0].comment === 'Done some test maintenance');
   });
 });
