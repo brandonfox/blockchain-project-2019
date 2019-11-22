95ce3781b7db27fcb102bf5be25d734050190c96
Sorawit Kongnurat <trainer.mapring@gmail.com>
2019-11-23 02:40:41 +0700

    start check.html to check applicants detail

--- public/js/check.js
+++ public/js/check.js
@@ -4,11 +4,14 @@ const initApp = async () => {
   const _userContract = await userContract.deployed();
   const accounts = await web3.eth.getAccounts();
   const application = await _userContract.getAllDealerApplications({ from: accounts[0] });
-  const node = document.createElement('p');
   console.log('application', application);
-  const textNode = document.createTextNode(`application, ${application}`);
-  node.appendChild(textNode);
-  document.getElementById('container').appendChild(node);
+  const userInfo = await _userContract.getDealerInfo("0x4b80ed9b45be1f5eda5a49be86a2b59de0be90ec6593c2bb00be1634e60ddf12", {from: accounts[0]});
+  console.log('userInfo', userInfo);
+  application.forEach(el => {
+    let node = document.createElement('p');
+    node.innerHTML = el;
+    document.body.appendChild(node);
+  })
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
