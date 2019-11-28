09397bee869b7378527e13e80a93e437cb3f424b
Sorawit Kongnurat <trainer.mapring@gmail.com>
2019-11-28 16:51:55 +0700

    use const for unchanged variables

--- public/js/userContract.js
+++ public/js/userContract.js
@@ -2,22 +2,21 @@
 import HDWalletProvider from '@truffle/hdwallet-provider';
 import Web3 from 'web3';
 import UserContract from '../../build/contracts/UserContract.json';
+
 const line = require('@line/bot-sdk');
 const contract = require('truffle-contract');
 
 export let web3;
 export let userContract;
-export let DealerClient;
-export let UserClient;
 
-DealerClient = new line.Client({
+export const DealerClient = new line.Client({
   channelAccessToken:
-    'zCyEDn4jZWQ5n7CPdK8lIy0leAQoE5QF3/uY53ND6hQP4C45g9royk/A8r7/p4PhJ9CKEjVgICZ0m7yH8RbX0is5UfMeogWS/Gxhfn3Q7U9Ry9/z8IWeEHjvHmeoSJjXEC/AmfcLUFYpaF0Ecdn5QgdB04t89/1O/w1cDnyilFU='
+    'zCyEDn4jZWQ5n7CPdK8lIy0leAQoE5QF3/uY53ND6hQP4C45g9royk/A8r7/p4PhJ9CKEjVgICZ0m7yH8RbX0is5UfMeogWS/Gxhfn3Q7U9Ry9/z8IWeEHjvHmeoSJjXEC/AmfcLUFYpaF0Ecdn5QgdB04t89/1O/w1cDnyilFU=',
 });
 
-UserClient = new line.Client({
+export const UserClient = new line.Client({
   channelAccessToken:
-    'hwM3ymw21vj2lhcWZhjvxWtN3XizPUctMfdh1tx8RrY5yhL5AYZMLR3dfNBa2RyFIEWmyxToyGXoOoEUwEcaNBjZZvD2eemiFYIcLCRCmRb9ERVhAWa1Olpydc4PGXstmwYBRYUSOOaroHX8VpTTVAdB04t89/1O/w1cDnyilFU='
+    'hwM3ymw21vj2lhcWZhjvxWtN3XizPUctMfdh1tx8RrY5yhL5AYZMLR3dfNBa2RyFIEWmyxToyGXoOoEUwEcaNBjZZvD2eemiFYIcLCRCmRb9ERVhAWa1Olpydc4PGXstmwYBRYUSOOaroHX8VpTTVAdB04t89/1O/w1cDnyilFU=',
 });
 
 const initContract = async () => {
