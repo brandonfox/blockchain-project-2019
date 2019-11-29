5adad90646b350016face801f7b957b612f92626
Natthawit Janjaroen <natwit442@gmail.com>
2019-11-29 22:54:51 +0700

    functions notify adjust

--- functions/src/line/users/notification.ts
+++ functions/src/line/users/notification.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
 import * as Web3Utils from '../../web3/index';
+// import * as UserLine from '../users/index';
+
+// const UserClient = UserLine.UserClient;
 
 export const notify = functions.pubsub
-  .schedule('every one mins')
+  .schedule('every 1 mins')
   .timeZone('Asia/Bangkok')
   .onRun(async context => {
     const instance = await Web3Utils.initWeb3();
