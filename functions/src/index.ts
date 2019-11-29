96c1c6a3cb434afc5f8489cf48ddc479282257ba
Natthawit Janjaroen <natwit442@gmail.com>
2019-11-29 19:45:16 +0700

    delete an unnecessary function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,4 @@
 import * as Line from './line/index';
-import * as functions from 'firebase-functions';
 
 // Real names of functions decleared here
 
@@ -7,11 +6,3 @@ import * as functions from 'firebase-functions';
 export const d_confirm = Line.confirmationHook;
 export const u_webhook = Line.userMainHook;
 export const u_getDb = Line.getDB;
-
-export const s = functions
-  .region('asia-east2')
-  .https.onCall((data, context) => {
-    console.log('data', data);
-    console.log('context', context);
-    return 'love';
-  });
