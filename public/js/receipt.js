78b1a2be354515a4ad345aa6113087d8ef400530
Sorawit Kongnurat <trainer.mapring@gmail.com>
2019-11-29 16:20:49 +0700

    remove verification.js and change label to Thai

--- public/js/receipt.js
+++ public/js/receipt.js
@@ -48,10 +48,12 @@ const initApp = async () => {
       const carDetails = {
         brand: document.getElementById('carBrand').value,
         model: document.getElementById('carModel').value,
-        year: document.getElementById('carYear').value,
+        year: document.getElementById('carYear').value
       };
       console.log(carDetails);
-      _userContract.editCarDetails(_userId, carPlate, carDetails, {from: accounts[0]});
+      _userContract.editCarDetails(_userId, carPlate, carDetails, {
+        from: accounts[0]
+      });
     }
     if (result.receipt.status) {
       const dealerRecords = await db
@@ -77,7 +79,7 @@ const initApp = async () => {
           services,
           subServices,
           comment,
-          date: new Date().getTime(),
+          date: new Date().getTime()
         });
       console.log('Added dealer record');
       alert('การทำรายการสำเร็จ');
@@ -130,7 +132,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   _userId = await _userContract.getHash('Yes');
   dealerId = _userId;
   console.log(_userId);
-  const carPlates = await _userContract.getCarPlates(_userId)
+  const carPlates = await _userContract.getCarPlates(_userId);
   let cars = '';
   for (let i = 0; i < carPlates.length; i++) {
     cars += `<option value="${carPlates[i]}"></option>`;
@@ -147,39 +149,46 @@ document.getElementById('receipt').addEventListener('submit', async e => {
 let gotDetailsBool = false;
 let gotRecordsBool = false;
 
-function gotDetails(callerPlate, data){
+function gotDetails(callerPlate, data) {
   const v = document.getElementById('car-plate').value;
-  if(v === callerPlate){
+  if (v === callerPlate) {
     userCarDetails = data;
-    let carDetails = "";
-    if(data.brand !== ""){
-      carDetails += '<div class="form-row"><label for="carBrand">Car Brand</label>';
+    let carDetails = '';
+    if (data.brand !== '') {
+      carDetails +=
+        '<div class="form-row"><label for="carBrand">ยี่ห้อรถ</label>';
       carDetails += `<input id="carBrand" name="carBrand" type="text" value="${userCarDetails.brand}" readonly disabled/>`;
       carDetails += '</div>';
-      carDetails += '<div class="form-row"><label for="carModel">Car Model</label>';
+      carDetails +=
+        '<div class="form-row"><label for="carModel">รุ่นของรถ</label>';
       carDetails += `<input id="carModel" name="carModel" type="text" value="${userCarDetails.model}" readonly disabled/>`;
       carDetails += '</div>';
-      carDetails += '<div class="form-row"><label for="carYear">Car Year</label>';
+      carDetails +=
+        '<div class="form-row"><label for="carYear">ปีที่ผลิตรถ</label>';
       carDetails += `<input id="carYear" name="carYear" type="text" value="${userCarDetails.year}" readonly disabled/>`;
       carDetails += '</div>';
-      carDetails += '<div class="form-row"><button style="width: 100%; height: 40px;" type="button" onclick="openModal()">View record</button></div>'
+      carDetails +=
+        '<div class="form-row"><button style="width: 100%; height: 40px;" type="button" onclick="openModal()">ดูประวัติ</button></div>';
       document.getElementById('carDataArea').innerHTML += carDetails;
       newCar = false;
-    }
-    else{
-      carDetails += '<div class="form-row"><label for="carBrand">Car Brand</label>';
+    } else {
+      carDetails +=
+        '<div class="form-row"><label for="carBrand">Car Brand</label>';
       carDetails +=
         '<input id="carBrand" name="carBrand" type="text" value="" required/>';
       carDetails += '</div>';
-      carDetails += '<div class="form-row"><label for="carModel">Car Model</label>';
+      carDetails +=
+        '<div class="form-row"><label for="carModel">Car Model</label>';
       carDetails +=
         '<input id="carModel" name="carModel" type="text" value="" required/>';
       carDetails += '</div>';
-      carDetails += '<div class="form-row"><label for="carYear">Car Year</label>';
+      carDetails +=
+        '<div class="form-row"><label for="carYear">Car Year</label>';
       carDetails +=
         '<input id="carYear" name="carYear" type="text" value="" required/>';
       carDetails += '</div>';
-      carDetails += '<div class="form-row"><button style="width: 100%; height: 40px;" type="button" onclick="openModal()">View record</button></div>'
+      carDetails +=
+        '<div class="form-row"><button style="width: 100%; height: 40px;" type="button" onclick="openModal()">ดูประวัติ</button></div>';
       document.getElementById('carDataArea').innerHTML += carDetails;
       newCar = true;
     }
@@ -188,15 +197,15 @@ function gotDetails(callerPlate, data){
   }
 }
 
-function checkStatus(){
-  if(gotDetailsBool && gotRecordsBool){
+function checkStatus() {
+  if (gotDetailsBool && gotRecordsBool) {
     document.getElementById('retrieveDetailLoader').remove();
   }
 }
 
-function gotRecords(callerPlate, data){
+function gotRecords(callerPlate, data) {
   const v = document.getElementById('car-plate').value;
-  if(v === callerPlate){
+  if (v === callerPlate) {
     carRecords = data;
     populateModal(data);
     gotRecordsBool = true;
@@ -210,11 +219,11 @@ document.getElementById('car-plate').addEventListener('change', function() {
   gotDetailsBool = false;
   const el = document.getElementById('carDataArea');
   const v = document.getElementById('car-plate').value;
-  el.innerHTML = '<div id="retrieveDetailLoader" class="loader"></div>'
+  el.innerHTML = '<div id="retrieveDetailLoader" class="loader"></div>';
   _userContract.getRecords(v).then(result => {
-    gotRecords(v,result);
-  })
+    gotRecords(v, result);
+  });
   _userContract.getCarDetails(v).then(result => {
-    gotDetails(v,result);
-  })
+    gotDetails(v, result);
+  });
 });
